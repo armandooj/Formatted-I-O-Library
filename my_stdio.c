3a27dbd5b7ecb89786d5854ffca89d27c1c08659
Armando Ochoa <armando.o.j@gmail.com>
2014-10-17 15:38:05 +0200

    Wrote a small skeleton for my_fread

--- my_stdio.c
+++ my_stdio.c
@@ -1,27 +1,29 @@
 #include "my_stdio.h"
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 // Printing
 #include <stdio.h>
 
 #define BUFFER_SIZE 64
 
+void refill_buffer(MY_FILE *f);
+
+/*
+Opens an access to a file, where name is the path of the file and mode is either ”r” for read or ”w” for write. 
+Returns a pointer to a MY FILE structure upon success and NULL otherwise.
+*/
 MY_FILE *my_fopen(char *name, char *mode) {
-			printf("OPEN ");
 	// Here we need to initialize the buffer
 	// Open + what is needed to initialize the structure
 	MY_FILE *new_file = (MY_FILE *)malloc(sizeof(MY_FILE));
 	new_file->buffer = (char *)malloc(sizeof(char)*BUFFER_SIZE);
 	new_file->name = name;
 	new_file->mode = mode;
-	// Read from the beginning
-	new_file->offset = 0;
-	new_file->end_buffer = 0;
+	new_file->buff_offset = 0;
 
 	// Open the file -> kernel mode
-	printf("* %s *\n", name);
-
 	int flag;
 	if (*mode == 'r') {
 		flag = O_RDONLY;
@@ -32,39 +34,87 @@ MY_FILE *my_fopen(char *name, char *mode) {
 	}
 
 	int inf = open(name, flag);
-	if (inf == -1) {
+	if (inf < 0) {
 		printf("Error opening file %s.\n", name);
 		return NULL;
-		}
-	new_file->fd = inf;
-		printf("OPEN OK\n");
+	} else {
+		// Success, save the file descriptor for future access to the file
+		new_file->fd = inf;
+
+		// Should this be here?
+		refill_buffer(new_file);
+	}
+	
 	return new_file;
 }
 
-// TODO group the reads and the reads in groups of 64 bytes 
-// every 64 bytes we'll have a system call
+void refill_buffer(MY_FILE *f) {
+	// TODO validate it's not < 0
+	read(f->fd, f->buffer, BUFFER_SIZE);
+}
 
+/*
+Closes the access to a file associated to f. Returns 0 upon success and -1 otherwise.
+*/
 int my_fclose(MY_FILE *f) {
+	// TODO group the reads and the reads in groups of 64 bytes 
+	// every 64 bytes we'll have a system call
 	return 0;
 }
 
+/*
+Reads at most nbelem elements of size size from file access f, that has to have been opened with mode ”r”, and places them at the address pointed by p. 
+Returns the number of elements actually read, 0 if an end-of-file has been encountered before any element has been read and -1 if an error occured.
+*/
 int my_fread(void *p, size_t size, size_t nbelem, MY_FILE *f) {
 	
-	return read(f->fd,p,size*nbelem);
-
+	if (*f->mode == 'r') {
+		char *content;
+		// Proceed reading one by one
+		while (nbelem > 0) {
+			// Try to read from the buffer
+			if (BUFFER_SIZE - f->buff_offset >= (int)size) {
+				/*
+				char temp[(int)size];
+				memcpy(temp, f->buffer[f->buff_offset], (int)size);
+				// TODO end of line?
+				strcat(content, temp);
+				*/
+				// try +=
+			} 
+			// Not enough data in the buffer. We must "reload" it
+			else {
+				refill_buffer(f);	
+			}
+			p = content;
+			nbelem--;			
+		}
+	} else {
+		printf("asdfasfasfasdf\n");
+		return -1;
+	}
+	
+	return read(f->fd, p, size * nbelem);
 }
+
 /*
 int doit(int fd, const void *buf, size_t count){
 	write(fd,buf,count);
 	return 0;
-	}*/
+}*/
 
+/*
+Writes at most nbelem elements of size size to file access f, that has to have been opened with mode ”w”, taken at the address pointed by p.
+Returns the number of elements actually written and -1 if an error occured.
+*/
 int my_fwrite(void *p, size_t size, size_t nbelem, MY_FILE *f) {
 	
-	return write(f->fd,p,size*nbelem);
-
+	return write(f->fd, p, size * nbelem);
 }
 
+/*
+Returns 1 if an end-of-file has been encountered during a previous read and 0 otherwise.
+*/
 int my_feof(MY_FILE *f) {
 	return 0;
 }
--- test_buffered.c
+++ test_buffered.c
@@ -20,7 +20,11 @@ int main (int argc, char *argv[])
   f2 = my_fopen(argv[2], "w");
   if (f2 == NULL)
       exit (-3);
+
+  result = my_fread(&c, 1, 4, f1);
+  result = my_fwrite(&c, 1, 1, f2);
  
+  /*
   result = my_fread(&c, 1, 1, f1);
   while (result == 1)
     {
@@ -33,5 +37,6 @@ int main (int argc, char *argv[])
       exit(-5);
   my_fclose(f1);
   my_fclose(f2);
+  */
   return 0;
 }
