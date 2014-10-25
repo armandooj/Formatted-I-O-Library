67ee3d7b584f1ed81faa0a4ebc38c494c5ce00ad
Armando Ochoa <armando.o.j@gmail.com>
2014-10-25 17:14:38 +0200

    Rewrote my_write to use the buffer in order to save sys calls

--- my_stdio.c
+++ my_stdio.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 // Printing
-// #include <stdio.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 64
 
@@ -25,14 +25,14 @@ MY_FILE *my_fopen(char *name, char *mode) {
 	new_file->found_eof = 0;
 	new_file->found_eob = 0;
 
-	// An offset of 0 is going to mean that we haven't read from the file yet
-	new_file->buff_offset = -1;
-
 	// Open the file -> kernel mode
 	int flag;
 	if (*mode == 'r') {
+		// An offset of 0 is going to mean that we haven't read from the file yet
+		new_file->buff_offset = -1;
 		flag = O_RDONLY;
 	} else if (*mode == 'w') {
+		new_file->buff_offset = 0;
 		flag = O_WRONLY | O_CREAT;
 	} else {
 		flag = 0;
@@ -72,82 +72,89 @@ Returns the number of elements actually read, 0 if an end-of-file has been encou
 int my_fread(void *p, size_t size, size_t nbelem, MY_FILE *f) {
 
 	if (*f->mode == 'r') {
-		int no_elements = nbelem;
-		int content_offset = 0;
-		int found_eof = 0;
-		char *content = (char *)p;
-		content[0] = '\0';
-		
-		// Proceed reading one by one
-		while (no_elements > 0) {
-
-			// First time - check if no element has bean read and we encountered an end of file
-			if (f->buff_offset == -1) {
-				found_eof = refill_buffer(f);
-				if (found_eof == 2) {
-					return -1;
-				}
+		// TODO (maybe)
+		if (size > BUFFER_SIZE) {
+			while (nbelem > 0) {
+				read(f->fd, p, size);
+				nbelem--;
 			}
-
-			// Not enough data in the buffer. We must "reload" it
-			if (BUFFER_SIZE - f->buff_offset < (int)size) {
-				if (found_eof == 1) break;
-				found_eof = refill_buffer(f);
-			}
-
-			char temp[(int)size + 1];
+		} else {
+			int no_elements = nbelem;
+			int content_offset = 0;
+			int found_eof = 0;
+			char *content = (char *)p;
+			content[0] = '\0';
 			
-			// Try to read from the buffer 1 element of size size
-			int x = 0;
-			for (int i = f->buff_offset; i < f->buff_offset + size; i++) {
-				// End of file
-				if (f->buffer[i] == '\0') {
-					// Both end of buffer and end of file
-					if (f->found_eof == 1) {
-						f->found_eob = 1;
+			// Proceed reading one by one
+			while (no_elements > 0) {
+
+				// First time - check if no element has bean read and we encountered an end of file
+				if (f->buff_offset == -1) {
+					found_eof = refill_buffer(f);
+					if (found_eof == 2) {
+						return -1;
 					}
+				}
+
+				// Not enough data in the buffer. We must "reload" it
+				if (BUFFER_SIZE - f->buff_offset < (int)size) {
+					if (found_eof == 1) break;
+					found_eof = refill_buffer(f);
+				}
+
+				char temp[(int)size + 1];
 				
-					no_elements = 0;					
-					// For single chars, we need to return when end of line is found (nothing else to read)
-					if (size == 1 && nbelem == 1) {	
-						content[content_offset] = f->buffer[i];		
-						memcpy(p, content, 1);			
-						return 0;
+				// Try to read from the buffer 1 element of size size
+				int x = 0;
+				for (int i = f->buff_offset; i < f->buff_offset + size; i++) {
+					// End of file
+					if (f->buffer[i] == '\0') {
+						// Both end of buffer and end of file
+						if (f->found_eof == 1) {
+							f->found_eob = 1;
+						}
+					
+						no_elements = 0;					
+						// For single chars, we need to return when end of line is found (nothing else to read)
+						if (size == 1 && nbelem == 1) {	
+							content[content_offset] = f->buffer[i];		
+							memcpy(p, content, 1);			
+							return 0;
+						}
+
+						break;
 					}
 
-					break;
+					temp[x] = f->buffer[i];
+					x++;
+				}
+				temp[x] = '\0';
+
+				// Update the buffer's and content's offset position
+				// We need two variables because the buffer's offset is set to 0 when read is called
+				f->buff_offset += (int)size;
+				content_offset += (int)size;
+				
+				// Concatenate every temp element in content
+				// content = concat(content, temp);
+				if (size == 1 && nbelem == 1) {
+					content = temp;
+				} else {
+					content = strcat(content, temp);
 				}
 
-				temp[x] = f->buffer[i];
-				x++;
-			}
-			temp[x] = '\0';
+				content[content_offset] = '\0';		
 
-			// Update the buffer's and content's offset position
-			// We need two variables because the buffer's offset is set to 0 when read is called
-			f->buff_offset += (int)size;
-			content_offset += (int)size;
-			
-			// Concatenate every temp element in content
-			// content = concat(content, temp);
-			if (size == 1 && nbelem == 1) {
-				content = temp;
-			} else {
-				content = strcat(content, temp);
-			}
+				no_elements--;		
 
-			content[content_offset] = '\0';		
-
-			no_elements--;		
-
-			// For single chars, assign to p only the char - without the end of line
-			if (size == 1 && nbelem == 1) {
-				memcpy(p, content, 1);
-			} else {
-				memcpy(p, content, size * nbelem + 1);
+				// For single chars, assign to p only the char - without the end of line
+				if (size == 1 && nbelem == 1) {
+					memcpy(p, content, 1);
+				} else {
+					memcpy(p, content, size * nbelem + 1);
+				}
 			}
 		}
-
 		return nbelem;
 	} else {
 		// Dennied access
@@ -161,7 +168,44 @@ Returns the number of elements actually written and -1 if an error occured.
 */
 int my_fwrite(void *p, size_t size, size_t nbelem, MY_FILE *f) {
 	if (*f->mode == 'w') {
-		return write(f->fd, p, size * nbelem);
+		// We can't write to buffer, write to the file
+		if (size > BUFFER_SIZE) {
+			return write(f->fd, p, size * nbelem);
+		} else {
+			//return write(f->fd, p, size * nbelem);
+			char *content = (char *)p;
+			int chunk = 0;
+
+			while ((int)nbelem > 0) {
+				// Not enough space in buffer
+				if (BUFFER_SIZE - f->buff_offset < size) {
+
+					// Write to file what buffer had
+					write(f->fd, f->buffer, BUFFER_SIZE - (BUFFER_SIZE - f->buff_offset));
+					f->buff_offset = 0;
+					f->buffer[0] = '\0';
+				} 
+
+				// There's enough space in the buffer, write to it
+				int i;
+				for (i = 0; i < size; i++) {
+					f->buffer[f->buff_offset] = content[chunk * size + i];
+					f->buff_offset++;				
+				}
+				
+				chunk++;
+				nbelem--;						
+			}
+			
+			// Write whatever is left
+			if (f->buffer[0] != '\0') {
+				write(f->fd, f->buffer, BUFFER_SIZE - (BUFFER_SIZE - f->buff_offset));
+				f->buff_offset = 0;
+				f->buffer[0] = '\0';
+			}
+
+			return nbelem * size;
+		}
 	}
 	else {
 		// Dennied access
